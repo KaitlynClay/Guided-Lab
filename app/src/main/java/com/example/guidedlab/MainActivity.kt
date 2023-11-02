d925655f5b9d7cf6d510ba7a7b818229d5aa42b5
Kaitlyn Clay <kclay@students.mchenry.edu>
2023-11-02 17:47:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/guidedlab/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.guidedlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    enum class LoginSuccess
+        constructor(val intVal: Int) {
+            login(1),
+            password(2),
+            success(0)
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txtLogin = findViewById<TextView>(R.id.idUsernameEntry)
+        val txtPassword = findViewById<TextView>(R.id.idPasswordEntry)
+        val btnLogin = findViewById<Button>(R.id.idBtn)
+
+        btnLogin.setOnClickListener {
+            when (CheckLogin(txtLogin.text.toString(), txtPassword.text.toString())){
+                LoginSuccess.login -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessageLogin), Toast.LENGTH_LONG).show()
+                    txtLogin.requestFocus()
+                }
+                LoginSuccess.password -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessagePassword), Toast.LENGTH_LONG).show()
+                    txtPassword.requestFocus()
+                }
+                else ->
+                    Toast.makeText(applicationContext, "Success", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun CheckLogin(txtLogin: String, txtPassword: String): LoginSuccess {
+        val holdLogin = "Kaitlyn"
+        val holdPass = "password"
+
+        return if (txtLogin != holdLogin) {
+            LoginSuccess.login
+        }else if (txtPassword != holdPass) {
+            LoginSuccess.password
+        }else {
+            LoginSuccess.success
+        }
+    }
+}
